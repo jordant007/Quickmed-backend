ecdc9b569157b2675bfbd2a7bf44d1056deb2a2a
Jordant007 <jordantyrill@gmail.com>
2025-02-05 11:23:23 +0300

    fixed the error on delete method

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', DELETE, 'OPTIONS']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 // Body Parser Middleware
